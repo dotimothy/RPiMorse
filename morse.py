3222ce2b630f7b5267538152d9002d86f245d9ee
Timothy Do <timothydobsa@gmail.com>
2020-09-20 23:14:54 -0700

    defining functions pt 1

--- morse.py
+++ morse.py
@@ -1,1 +1,24 @@
+#author Timothy Do
+#RPiMorse Console: Prints Input in Morse Code to Console
+
+#Libraries
+import time
+
+
+# Main Function, Prompt of Program
+feed = ""
 def main():
+    print("Welcome to RPiMorse Console Version! It will convert your input into Morse Code and print to console.")
+    feed = input("Type what you want to convert and press enter: ")
+    print("Your input is " + feed + ".") 
+    time.sleep(1)
+    print("Here is your input in Morse Code!")
+    time.sleep(1)
+    printMorse(feed)
+
+def printMorse(feed):
+    for character in feed:
+        print(character)
+
+main()
+
