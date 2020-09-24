367483e943d2ed9ef26552325214e0637337d988
Timothy Do <timothd4@uci.edu>
2020-09-24 12:32:58 -0700

    Update morseled.py

--- morseled.py
+++ morseled.py
@@ -1,2 +1,2 @@
-#Run morseled.py ONLY IF you designed a similar circuit with an LED, resistor, breadboard, and a Raspberry Pi as described in README.md
+#Intended to be only with the RPi, read disclaimer.
 print("DISCLAIMER: Run morseled.py ONLY IF you designed a similar circuit with an LED, resistor, breadboard, and a Raspberry Pi as described in README.md")
