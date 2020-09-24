0629ef40aeeae316b678f710dae4d2c71a8f5098
Timothy Do <timothydobsa@gmail.com>
2020-09-24 16:12:48 -0700

    Update morseled.py

--- morseled.py
+++ morseled.py
@@ -3,4 +3,17 @@ try:
 except ModuleNotFoundError: 
 	print("You don't have GPIO Pins, so you can't run this program! Redirecting to console version...")
 
+#Board Setup
 GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+#Set LED pin to pin 11
+led = 11
+GPIO.setup(led,GPIO.out)
+while True:
+	GPIO.output(led,True)
+	sleep(1)
+	GPIO.output(led,False)
+	sleep(1)
+
+
+
