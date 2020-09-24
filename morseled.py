e1dbfe14062799a414a9e0e8b19e57e33b79b9f7
Timothy Do <timothydobsa@gmail.com>
2020-09-24 16:07:01 -0700

    Update morseled.py

--- morseled.py
+++ morseled.py
@@ -1,7 +1,6 @@
 try:
 	import RPi.GPIO as GPIO
 except ModuleNotFoundError: 
-	print("You don't have GPIO Pins, so you can't run this program! Redirecting to console version...)
-	import morse
+	print("You don't have GPIO Pins, so you can't run this program! Redirecting to console version...")
 
 GPIO.setwarning(False)
