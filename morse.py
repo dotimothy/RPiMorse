ff7e78b68701f0d68ff520f2a2d863bc3cf196e7
Timothy Do <timothydobsa@gmail.com>
2020-09-24 02:19:16 -0700

    Update morse.py

--- morse.py
+++ morse.py
@@ -1,11 +1,11 @@
 import sys
-#import morse2
+import morse2
 #import morse3
 
 version = sys.version_info[0]
 if(version == 2):
     print("hi")
-    #morse2.main()
+    morse2.main()
 elif(version == 3):
     print("bye")
     #morse3.main()
